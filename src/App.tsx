fe97a837f2e23c100489b9ec01e9973302d569b0
Людмила Алтухова <luda.tamb@mail.ru>
2023-01-16 17:22:39 +0300

    feat: implement basic main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+import Vendor from "./components/Vendor";
+
+const AppWrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: url("./images/bg.jpg");
+`;
 
 const App = () => {
-  return <></>;
+  return (
+    <AppWrapper>
+      <Vendor />
+    </AppWrapper>
+  );
 };
 
 export default App;
