3c99fd64f313c9b73f32e1b05b76766a8f2202d6
Людмила Алтухова <luda.tamb@mail.ru>
2023-01-18 17:05:02 +0300

    refactor: correct main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 
+import ProductsProvider from "./context/productsContext";
 import Vendor from "./components/Vendor";
+import UserPanel from "./components/UserPanel";
 
 const AppWrapper = styled.div`
-  height: 100vh;
+  padding: 30px 0;
+  min-height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
+  gap: 100px;
   background-image: url("./images/bg.jpg");
 `;
 
-const App = () => {
+const App = (): JSX.Element => {
   return (
-    <AppWrapper>
-      <Vendor />
-    </AppWrapper>
+    <ProductsProvider>
+      <AppWrapper>
+        <Vendor />
+        <UserPanel />
+      </AppWrapper>
+    </ProductsProvider>
   );
 };
 
